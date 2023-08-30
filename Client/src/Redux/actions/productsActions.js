f549cf007e8a78402222a326854f7c729eab3d2e
NiettoVale <nietto.program@gmail.com>
2023-08-29 22:19:48 -0300

    Modifcando unos pequeños detalles

--- Client/src/Redux/actions/productsActions.js
+++ Client/src/Redux/actions/productsActions.js
@@ -1,17 +1,15 @@
 import axios from "axios";
-import {
-    GET_PRODUCTS,
-} from "./actionTypes";
+import { GET_PRODUCTS } from "./actionTypes";
 
 export const getProducts = () => {
-    return async (dispatch) => {
-        try {
-            const response = await axios.get("http://localhost:3001/prendas");
-            const products = response.data;
-            dispatch ({ type: GET_PRODUCTS, payload: products });
-            return products;
-        } catch (error) {
-            console.error("Error fetching products:", error);
-        }
-    }; 
+  return async (dispatch) => {
+    try {
+      const response = await axios.get("http://localhost:3001/products");
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS, payload: products });
+      return products;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
 };
