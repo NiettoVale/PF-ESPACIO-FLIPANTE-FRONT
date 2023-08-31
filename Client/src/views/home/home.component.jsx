6ff20cd2848b5ae18896c9757536e4513a69b4c4
Ramirof <ramirofiscella@gmail.com>
2023-08-30 21:35:56 -0300

    added card styles

--- Client/src/views/home/home.component.jsx
+++ Client/src/views/home/home.component.jsx
@@ -4,6 +4,8 @@ import { getProducts } from "../../Redux/actions/productsActions";
 import Cards from "../../Components/cards/cards.component";
 import Hero from "../../Components/Hero/Hero";
 
+import styles from "./home.module.css";
+
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
@@ -14,9 +16,12 @@ const Home = () => {
   return (
     <div>
       <Hero />
-      <h1>ESPACIO FLIPANTE</h1>
-      <div className="cards-container">
-        <Cards products={products} />
+      <div className={styles.catalogueSection}>
+        <h2>Catalogo</h2>
+        <p className={styles.catalogueSectionP}>ropa flipante</p>
+        <div className={styles.cardsContainer}>
+          <Cards products={products} />
+        </div>
       </div>
     </div>
   );
