4065bf639c3fb85a10fed6e060363e5d51f87d0d
Ramirof <ramirofiscella@gmail.com>
2023-09-01 15:32:28 -0300

    updated navbar

--- Client/src/Components/NavBar/navBar.jsx
+++ Client/src/Components/NavBar/navBar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className={styles.navContainer}>
       <div className={styles.navBar}>
-        <div className={styles.genderNav}>
-          <a>CATALOGO</a>
-          <a>OFERTAS</a>
-        </div>
+        <Link to={"/home"}>
+          <a>INICIO</a>
+        </Link>
+        <a>CATALOGO</a>
+        <a>OFERTAS</a>
+      </div>
+
+      <div>
+        <a className={styles.link}>ACCESO</a>
+        <a className={styles.link}>CARRITO</a>
       </div>
     </div>
   );
