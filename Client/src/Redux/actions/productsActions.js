fb1f614209cc7959cbb9453522fa89c9c0404919
NiettoVale <nietto.program@gmail.com>
2023-09-01 17:07:25 -0300

    Agregando los filter y el paginate

--- Client/src/Redux/actions/productsActions.js
+++ Client/src/Redux/actions/productsActions.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import { GET_PRODUCTS, GET_PRODUCTSBYNAME, POST_PRODUCT, GET_SIZES } from "./actionTypes";
+import {
+  GET_PRODUCTS,
+  POST_PRODUCT,
+  GET_SIZES,
+  FILTER,
+  GET_CATEGORY,
+  GET_GENDER,
+  ORDER,
+} from "./actionTypes";
 
 export const getProducts = () => {
   return async (dispatch) => {
@@ -14,26 +22,13 @@ export const getProducts = () => {
   };
 };
 
-export const getProductsByName = (name) => {
-  return async (dispatch) => {
-    try {
-      console.log(name);
-      const productsByName = await axios.get(
-        `http://localhost:3001/filter?name=${name}`
-      );
-      console.log(productsByName);
-      dispatch({ type: GET_PRODUCTSBYNAME, payload: productsByName });
-      return productsByName;
-    } catch (error) {
-      return error;
-    }
-  };
-};
-
 export const postProduct = (productData) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post("http://localhost:3001/products", productData);
+      const response = await axios.post(
+        "http://localhost:3001/products",
+        productData
+      );
       const createdProduct = response.data;
       dispatch({ type: POST_PRODUCT, payload: createdProduct });
       return createdProduct;
@@ -47,10 +42,71 @@ export const getSizes = () => {
   return async (dispatch) => {
     try {
       const { data } = await axios("http://localhost:3001/sizes");
-      console.log(data, "soy data")
-      dispatch({ type: GET_SIZES, payload: data})
+      console.log(data, "soy data");
+      dispatch({ type: GET_SIZES, payload: data });
     } catch (error) {
       console.error(error);
     }
-  }
-}
+  };
+};
+
+export const getGenders = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios("http://localhost:3001/gender");
+      dispatch({ type: GET_GENDER, payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const getCategory = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios("http://localhost:3001/category");
+      dispatch({ type: GET_CATEGORY, payload: data });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const getFilters = (dataFilter) => {
+  return async (dispatch) => {
+    try {
+      // Verifica si todas las propiedades de dataFilter están vacías
+      const isDataFilterEmpty = Object.values(dataFilter).every(
+        (value) => value === ""
+      );
+
+      if (isDataFilterEmpty) {
+        // Si todas están vacías, envía un array vacío como payload
+        dispatch({ type: FILTER, payload: [] });
+        return; // Sal de la función
+      }
+
+      const response = await fetch("http://localhost:3001/filter", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataFilter),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 404) {
+        alert(data.message);
+      }
+
+      dispatch({ type: FILTER, payload: data });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const setOrderByName = (order) => {
+  return { type: ORDER, payload: order };
+};
