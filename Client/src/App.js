6d5c58917bc2a5ed927bac81f62aba3cb5c1dabf
NiettoVale <nietto.program@gmail.com>
2023-09-03 19:17:28 -0300

    C:/Program Files/Git/home

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./views/Detail/Detail";
 import Home from "./views/home/home.component";
-import CreateProduct from "./views/create/create.view";
+import CreateProduct from "./views/create/Create.view";
 import Registro from "./Components/RegisterUser/registerUser.component";
 import LoginForm from "./Components/Login/LoginForm.component";
 import "./App.css";
@@ -10,7 +10,7 @@ import "./App.css";
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/detail/:id" element={<Detail />} />
       <Route path="/create" element={<CreateProduct />} />
       <Route path="/login" element={<LoginForm />} />
