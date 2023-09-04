dfac295748d411b449c1166566ce2586a9980b29
Valentín Francisco Diaz Nieto <130608630+NiettoVale@users.noreply.github.com>
2023-09-04 11:17:23 -0300

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./views/Detail/Detail";
 import Home from "./views/home/home.component";
-import CreateProduct from "./views/create/Create.view";
+import CreateProduct from "./views/create/create.view";
 import Registro from "./Components/RegisterUser/registerUser.component";
 import LoginForm from "./Components/Login/LoginForm.component";
 import "./App.css";
